c06726d720e8c0f21c6d7d3a976abaf75c72c854
mazeeer <ajidagba21@gmail.com>
2022-01-09 16:22:28 +0100

    CSS folder

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
 
 //static files
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/public'));
 
 //routes
 app.use(router)
