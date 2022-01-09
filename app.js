15c6843de9d95575431cc553dbd566f2dff14e7b
mazeeer <ajidagba21@gmail.com>
2022-01-09 16:17:15 +0100

    Heroku Port

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
 
 //static files
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname));
 
 //routes
 app.use(router)
